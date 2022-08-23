102aaba31394e739cdf5b19069d649fc680201af
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-08-23 10:35:23 -0300

    Vehicle class

---
+++ src/main/java/com/yamamoto64/SeidorCar/domain/Vehicle.java
@@ -0,0 +1,63 @@
+package com.yamamoto64.SeidorCar.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Vehicle implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String color;
+	private String brand;
+	
+	public Vehicle () {
+	}
+
+	public Vehicle(Long id, String color, String brand) {
+		super();
+		this.id = id;
+		this.color = color;
+		this.brand = brand;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vehicle other = (Vehicle) obj;
+		return Objects.equals(id, other.id);
+	}
+}
