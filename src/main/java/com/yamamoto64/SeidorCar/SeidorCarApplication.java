a4f0ecffbec354925c535b3f964ade6a84cdc3bf
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-08-23 10:30:07 -0300

    project created

---
+++ src/main/java/com/yamamoto64/SeidorCar/SeidorCarApplication.java
@@ -0,0 +1,13 @@
+package com.yamamoto64.SeidorCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeidorCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeidorCarApplication.class, args);
+	}
+
+}
