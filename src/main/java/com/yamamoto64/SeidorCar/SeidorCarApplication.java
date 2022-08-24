00aece71f535471ec36ec059d918bfbd077a8779
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-08-24 14:30:07 -0300

    Adding more contracts to test parameters

--- src/main/java/com/yamamoto64/SeidorCar/SeidorCarApplication.java
+++ src/main/java/com/yamamoto64/SeidorCar/SeidorCarApplication.java
@@ -38,9 +38,9 @@ public class SeidorCarApplication implements CommandLineRunner{
 		Vehicle v2 = new Vehicle(null, "blue", "ford");
 		Vehicle v3 = new Vehicle(null, "yellow", "fiat");
 		
-		Driver d1 = new Driver(null, "Carlos");
-		Driver d2 = new Driver(null, "Maria");
-		Driver d3 = new Driver(null, "João");
+		Driver d1 = new Driver(null, "Roberto Carlos");
+		Driver d2 = new Driver(null, "Maria Antonieta");
+		Driver d3 = new Driver(null, "João Cabral");
 		
 		vehicleRepository.saveAll(Arrays.asList(v1, v2, v3));
 		driverRepository.saveAll(Arrays.asList(d1, d2, d3));
@@ -48,8 +48,13 @@ public class SeidorCarApplication implements CommandLineRunner{
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		Rent r1 = new Rent(null, sdf.parse("16/02/2022"), sdf.parse("17/02/2022"), d1, v1, "Aluguel de carro");
+		Rent r2 = new Rent(null, sdf.parse("18/02/2022"), sdf.parse("20/02/2022"), d1, v1, "Aluguel de carro");
+		Rent r3 = new Rent(null, sdf.parse("16/02/2022"), sdf.parse("19/02/2022"), d2, v3, "Aluguel de carro");
+		Rent r4 = new Rent(null, sdf.parse("16/02/2022"), sdf.parse("19/02/2022"), d3, v2, "Aluguel de carro");
+		Rent r5 = new Rent(null, sdf.parse("20/02/2022"), sdf.parse("21/02/2022"), d2, v2, "Aluguel de carro");
+
 		
-		rentRepository.save(r1);
+		rentRepository.saveAll(Arrays.asList(r1, r2, r3, r4, r5));
 	}
 	
 	
